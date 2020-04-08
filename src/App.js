1f862d3ad14ece279214a8cda409dfea597387e0
Elliot Kim <elliotkim916@gmail.com>
2020-04-08 14:38:04 -0700

    Imported LandingPage to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <h1>Checkered Board</h1>
+      <LandingPage />
     </div>
   );
 }
