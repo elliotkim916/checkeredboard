bd6d73fada8e2cdf8b7d8c99376a24d88bd164f9
Elliot Kim <elliotkim916@gmail.com>
2020-04-08 15:37:48 -0700

    Successfully rendering checkerboard with correct length

--- src/components/CheckerBoard/CheckerBoard.jsx
+++ src/components/CheckerBoard/CheckerBoard.jsx
@@ -3,12 +3,19 @@ import CheckerBoardCell from '../CheckerBoardCell/CheckerBoardCell';
 import './CheckerBoard.css';
 
 const CheckerBoard = ({ boardSize }) => {
-  let cell = <CheckerBoardCell />;
-
   if (boardSize) {
+    const checkerBoard = [];
+
+    for (let i = 0; i < Math.pow(parseInt(boardSize), 2); i++) {
+      checkerBoard.push(<CheckerBoardCell />);
+    }
+
     return (
-      <div className="container">
-        {cell}
+      <div 
+        className="container" 
+        style={{width: boardSize * 102, height: boardSize * 102}}
+      >
+        {checkerBoard}
       </div>
     );
   } else {
